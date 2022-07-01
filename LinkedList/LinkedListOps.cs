91ad83f04709e991d06790145d3abdf8f5ab16a7
soni240 <s.fegade21@gmail.com>
2022-07-02 01:04:54 +0530

    [Gunavanti]Add:Create Simple Linkedlist

---
+++ LinkedList/LinkedListOps.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedListOps
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+
+                }
+                temp.next = node;
+
+            }
+
+            Console.WriteLine(">>> {0} is added to LinkedList...", data);
+
+        }
+
+        internal void Display()
+        {
+            Node temp = this.head;
+
+            if (temp == null)
+            {
+                Console.WriteLine("\n>> Your LinkedList is Empty....");
+                return;
+            }
+            else
+            {
+                Console.Write("\nLinkedList :- { ");
+
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine("}");
+
+
+            }
+        }
+
+    }
+}
+
---
+++ LinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinkedList;
+
+Console.WriteLine("----- Welcome TO LinkedList -----\n");
+
+LinkedListOps list = new LinkedListOps();
+list.Add(56);
+list.Add(30);
+list.Add(70);
+list.Display();
+
+
