e8205406cb62729e4b68443754b429860179fd8a
soni240 <s.fegade21@gmail.com>
2022-07-02 01:27:43 +0530

    [Gunavanti]Add:insert item in between list

--- LinkedList/LinkedListOps.cs
+++ LinkedList/LinkedListOps.cs
@@ -9,6 +9,7 @@ namespace LinkedList
     public class LinkedListOps
     {
         internal Node head;
+        public int count = 0;
         internal void Add(int data)
         {
             Node node = new Node(data);
@@ -29,7 +30,7 @@ namespace LinkedList
                 temp.next = node;
 
             }
-
+            count++;
             Console.WriteLine(">>> {0} is added to LinkedList...", data);
 
         }
@@ -59,6 +60,45 @@ namespace LinkedList
             Console.WriteLine(">>> {0} is appended to LinkedList...", data);
 
 
+        }
+
+
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            Node newNode = new Node(data);
+
+            //if (position < 1)
+            //     Console.WriteLine("Invalid position");   // this will make linked list index start from 1 explicitly
+
+            if (position == 0)
+            {
+
+                newNode.next = head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = head;
+                while (position > 1)
+                {
+                    if (position <= count)
+                    {
+                        temp = temp.next;
+                        position--;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n>> LinkedList Index out of Bound !!!\nCan not Insert {0}", data);
+                        return head;
+                    }
+                }
+                newNode.next = temp.next;
+                temp.next = newNode;
+
+            }
+
+            Console.WriteLine("\n>>> {0} is inserted at {1} position in LinkedList....\n", data, position);
+            return head;
         }
 
         internal void Display()
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -5,6 +5,9 @@ Console.WriteLine("----- Welcome TO LinkedList -----\n");
 
 LinkedListOps list = new LinkedListOps();
 list.Add(56);
-list.Append(30);
+//list.Append(30);
 list.Append(70);
+list.InsertAtParticularPosition(1, 30);
 list.Display();
+
+
